89a2805491b36317b53b736c2fba832618aa961b
Maria-Sabino <83432324+Maria-Sabino@users.noreply.github.com>
2021-10-27 20:02:13 -0300

    Adicionar arquivos de projeto.

---
+++ classe/classe.cpp
@@ -0,0 +1,30 @@
+// classe.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+
+
+class MyClass
+{
+
+	public int N;
+	{
+		//inserindo variaveis 
+		static void Main(String[] args)
+		{
+			int N = int.Parse(Console.ReadLine)
+				double[] vect = new double[N];
+			//combinacao para criar um vetor
+			for (int N = 0; i < N; i++) {
+				vect[i] = double.Parse(Console.ReadLine()),
+					CultureInfo.InvariantCulture);
+			}
+
+			CultureInfo.InvariantCulture);
+		}
+		double sum = 0.0;
+
+		for (int i = 0; i < N; i++) {
+
+			sum += vect[i];
+		}
+	}
+
+}
